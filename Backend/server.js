031e19b74054b8111a4f4fcb8c6eaf96df42a420
maniacric <iamrishabhpal@gmail.com>
2021-09-14 01:16:54 +0530

    frontend created

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,21 @@ const app = require('express')();
 
 const server = require('http').createServer(app);
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(server,{
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+      }
+});
 
 io.on("connection",(socket)=>{
     console.log("what is socket:",socket);
     console.log("Socket is Active to get connected");
     socket.on("chat",(payload)=>{
             console.log("what is payload",payload);
-        io.emmit("chat",payload);
+        io.emit("chat",payload);
     })
 
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import {io} from 'socket.io-client'
+import {useState,useEffect} from 'react'
+import {nanoid} from "nanoid"
+const userName = nanoid(4);
+//no.env
+const socket = io.connect("http://localhost:3000")
+
 
 function App() {
+
+  const [message, setMessage] = useState("")
+  const [chat, setChat] = useState([])
+  const sendChat = (e) =>{
+    e.preventDefault()
+    socket.emit("chat",{message,userName});
+    setMessage('');
+  }
+
+  useEffect(() => {
+    socket.on("chat",(payload)=>{
+      setChat([...chat,payload])
+    })
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="App"  text-allign center>
+      <body>
+      <h1>Chat Application</h1>
+      {chat.map((payload,index)=>{
+          return (
+            <p key ={index}>{payload.message}: <span>id: {payload.userName}</span></p>
+          )
+        }
+      )}  
+      <form onSubmit={sendChat}>
+        <input type = "text" name = "chat" placeholder = "sendText" value = {message}
+        onChange = {(e) =>{
+          setMessage(e.target.value)
+        }}
+        />
+        <button type = "submit">Send</button>
+      </form>
+      </body>
+      </div>
+    
   );
 }
 
